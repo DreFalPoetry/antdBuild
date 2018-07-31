3d1d8af91346c9d11277a2cd12789a7f1dcf50dd
Racky <racky.liu@moca-tech.net>
2018-07-31 15:01:52 +0800

    添加新的页面

---
+++ antdDemo/src/components/LoginForm.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+import {Table,Popconfirm,Button} from 'antd';
+import {connect} from 'dva';
+
+class LoginForm extends Component{
+    render(){
+        return (
+            <div><Button>sdsd</Button></div>
+        )
+    }
+}
+
+export default connect()(LoginForm);
--- antdDemo/src/routes/IndexPage.js
+++ antdDemo/src/routes/IndexPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { connect } from 'dva';
 import styles from './IndexPage.css';
+import LoginForm from '../components/LoginForm';
 
 function IndexPage() {
   return (
     <div className={styles.normal}>
+    <LoginForm/>
       <h1 className={styles.title}>Yay! Welcome to dva!</h1>
       <div className={styles.welcome} />
       <ul className={styles.list}>
